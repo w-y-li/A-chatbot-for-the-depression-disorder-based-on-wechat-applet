4937827069ed8b930ec927b88eb7662b963ee607
李文豫 <93126839+lightxxxshadow@users.noreply.github.com>
2023-01-18 20:48:22 +0800

    Delete try.py

--- SDS_insertion/sds_get_score/try.py
+++
@@ -1,8 +0,0 @@
-import random
-
-data_=open("data_set/sds_q1_train.txt",encoding='utf-8')
-data=data_.readlines()
-random.shuffle(data)
-wdata=open("data_set/1.txt",'w',encoding='utf-8')
-for i in range(len(data)):
-    wdata.write(data[i])
